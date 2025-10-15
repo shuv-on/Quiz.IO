6b3ea1ecfc0319668b7ffe416713474cf31f76cb
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2025-10-15 12:41:29 +0600

    Loading spinner

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import NavBar from '../../components/NavBar/NavBar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Footer from '../Footer/Footer';
+import ideaImg from '../../assets/idea.svg'
 
 const Root = () => {
+    const location= useLocation();
+    const [loading, setLoading] = useState(true);
+    const loadingTimeRef = useRef(null);
+    const showFooter = ['/', '/quiz', '/leaderboard', '/about'].includes(location.pathname)
+
+useEffect(() => {
+        const initialTimer = setTimeout(() => {
+            setLoading(false);
+        }, 1000);
+        return () => clearTimeout(initialTimer);
+    }, []);
+
+    //start loading
+    const startLoading = () => {
+        setLoading(true);
+        if (loadingTimeRef.current) {
+            clearTimeout(loadingTimeRef.current);
+        }
+        loadingTimeRef.current = setTimeout(() => {
+            setLoading(false);
+            loadingTimeRef.current = null;
+        }, 1000);
+
+    };
+
     return (
         <div>
             <div>
@@ -13,8 +39,22 @@ const Root = () => {
                 <Outlet/>
             </main>
             <div>
-                <Footer></Footer>
+                {showFooter && <Footer></Footer>} 
             </div>
+            {loading && (
+                <div className='fixed inset-0 flex items-center justify-center bg-gray-100/75 backdrop-blur-sm z-50'>
+                    <div className="flex items-center gap-1">
+                    <span className="text-8xl font-semibold text-gray-400">L</span>
+                    <img
+                        src={ideaImg}
+                        
+                        className="h-16 animate-spin" 
+                    />
+                    <span className="text-8xl font-semibold text-gray-400">oading</span>
+                   </div>
+                </div>
+                
+            )}
             
         </div>
     );
